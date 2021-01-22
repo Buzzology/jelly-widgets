4e60a002377cee6402092dca9315281d3661b840
Buzzology <chrisowens@live.com.au>
2021-01-22 18:26:23 +1000

    [ADD] Added subscription management integration events library

---
+++ SubscriptionManagementIntegrationEvents/SiteCustomer/SiteCustomerCreatedIntegrationEvent.cs
@@ -0,0 +1,14 @@
+﻿using MicroservicesProjectLibrary.EventHandling.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SubscriptionManagementIntegrationEvents.SiteCustomer
+{
+    public class SiteCustomerCreatedIntegrationEvent : IntegrationEvent
+    {
+        public SubscriptionManagementData.Models.SiteCustomer User { get; set; }
+
+        public SiteCustomerCreatedIntegrationEvent(SubscriptionManagementData.Models.SiteCustomer user) => User = user;
+    }
+}
---
+++ SubscriptionManagementIntegrationEvents/SiteCustomer/SiteCustomerUpdatedIntegrationEvent.cs
@@ -0,0 +1,11 @@
+﻿using MicroservicesProjectLibrary.EventHandling.Events;
+
+namespace SubscriptionManagementIntegrationEvents.SiteCustomer
+{
+    public class SiteCustomerUpdatedIntegrationEvent : IntegrationEvent
+    {
+        public SubscriptionManagementData.Models.SiteCustomer SiteCustomer { get; set; }
+
+        public SiteCustomerUpdatedIntegrationEvent(SubscriptionManagementData.Models.SiteCustomer siteCustomer) => SiteCustomer = siteCustomer;
+    }
+}
