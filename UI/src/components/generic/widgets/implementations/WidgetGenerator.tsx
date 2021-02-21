97992bfefc1adf45d007a4c4af772c79900ec613
Buzzology <chrisowens@live.com.au>
2021-02-21 19:29:46 +1000

    [UX] Add widget not found placeholder

--- UI/src/components/generic/widgets/implementations/WidgetGenerator.tsx
+++ UI/src/components/generic/widgets/implementations/WidgetGenerator.tsx
@@ -1,13 +1,14 @@
-import { Typography } from "@material-ui/core";
 import React from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../../redux";
 import { selectorGetDashboardWidgetById } from "../../../../redux/dashboardWidget/selectors";
 import { WidgetIds } from "../../../../utilities/Constants";
+import { WidgetNoResultsPlaceholder } from "../WidgetNoResultsPlaceholder";
 import WidgetAustralianBusinessNumberGenerator from "./WidgetAustralianBusinessNumberGenerator";
 import WidgetAustralianBusinessNumberValidator from "./WidgetAustralianBusinessNumberValidator";
 import WidgetTaxFileNumberGenerator from "./WidgetTaxFileNumberGenerator";
 import WidgetTaxFileNumberValidator from "./WidgetTaxFileNumberValidator";
+import MissingIcon from "@material-ui/icons/NotInterested"
 
 interface IWidgetGenerator {
     widgetId: string,
@@ -36,7 +37,14 @@ function WidgetGenerator({ widgetId, dashboardWidgetId }: IWidgetGenerator) {
 
 
 function WidgetNotFound() {
-    return <Typography>Widget not found.</Typography>;
+    return (
+        <WidgetNoResultsPlaceholder
+            icon={MissingIcon}
+            text="Not found"
+            description="This widget no longer appears to be available."
+            fade={true}
+        />
+    );
 }
 
 
