b47e9b184a61588f252821d36bc0e30e6041f2c3
Buzzology <chrisowens@live.com.au>
2021-04-01 08:06:04 +1000

    [CHORE] Move pipeline into infrastructure folder

--- Infrastructure/scripts/InvokeDeployment.ps1
+++ Infrastructure/scripts/InvokeDeployment.ps1
@@ -1,4 +1,9 @@
 # This script actually does the deployment. See the readme for more info.
+
+# Used to copy environment variables to a file so that they are more accessible to subsequent steps
+ls
+"$env:PipelineInfrastructure" | Out-File parameters/parameters.prod.json
+
 Write-Host "List Files"
 ls *
 
--- Infrastructure/scripts/WriteParametersToFile.ps1
+++
@@ -1,3 +0,0 @@
-# Used to copy environment variables to a file so that they are more accessible to subsequent steps
-ls
-"$env:PipelineInfrastructure" | Out-File parameters/parameters.prod.json
