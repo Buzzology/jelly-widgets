46a7d2d1c2d7e06d0c6b59470f0af1a0e3946d17
Buzzology <chrisowens@live.com.au>
2021-03-31 18:56:13 +1000

    Update WriteParametersToFile.ps1

--- Infrastructure/scripts/WriteParametersToFile.ps1
+++ Infrastructure/scripts/WriteParametersToFile.ps1
@@ -1,1 +1,3 @@
-"$env:PipelineInfrastructure" | Out-File ./parameters/parameters.prod.json
+# Used to copy environment variables to a file so that they are more accessible to subsequent steps
+ls
+"$env:PipelineInfrastructure" | Out-File parameters/parameters.prod.json
