71896decee2dd8b73db24c5547f71d724ac13761
Buzzology <chrisowens@live.com.au>
2021-04-01 08:28:14 +1000

    Debug

--- Infrastructure/scripts/InvokeDeployment.ps1
+++ Infrastructure/scripts/InvokeDeployment.ps1
@@ -1,16 +1,2 @@
 # This script actually does the deployment. See the readme for more info.
-
-# Used to copy environment variables to a file so that they are more accessible to subsequent steps
-ls
-"$env:PipelineInfrastructure" | Out-File parameters/parameters.prod.json
-
-Write-Host "List Files"
-ls *
-
-Write-Host "Getting content:"
-get-content "parameters/parameters.prod.json"
-${parameters/parameters.prod.json}
-${$PipelineInfrastructure}
-
-
 az deployment group create --template-file ./main.bicep  --parameters "parameters/parameters.prod.json" -g "widgets-prod"
---
+++ Infrastructure/scripts/WriteParametersToFile.ps1
@@ -0,0 +1,11 @@
+# Used to copy environment variables to a file so that they are more accessible to subsequent steps
+ls
+"$env:PipelineInfrastructure" | Out-File parameters/parameters.prod.json
+
+Write-Host "List Files"
+ls *
+
+Write-Host "Getting content:"
+get-content "parameters/parameters.prod.json"
+${parameters/parameters.prod.json}
+${$PipelineInfrastructure}
