cb51b239bc29f411cd5e003f45cd7cfaa6523e94
Buzzology <chrisowens@live.com.au>
2021-03-31 19:25:29 +1000

    [DEBUG] More testing

--- Infrastructure/scripts/InvokeDeployment.ps1
+++ Infrastructure/scripts/InvokeDeployment.ps1
@@ -1,4 +1,8 @@
 # This script actually does the deployment. See the readme for more info.
+Write-Host "parameters/parameters.prod.json"
+ls "parameters/*"
+ls *
+
 Write-Host "Getting content:"
 get-content parameters/parameters.prod.json
 
