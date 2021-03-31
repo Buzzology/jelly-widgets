1a9599150402d9335f92f294521f9abe3222d24c
Buzzology <chrisowens@live.com.au>
2021-03-31 19:16:33 +1000

    [TEST] View contents of env vars

--- Infrastructure/scripts/InvokeDeployment.ps1
+++ Infrastructure/scripts/InvokeDeployment.ps1
@@ -1,1 +1,6 @@
+# This script actually does the deployment. See the readme for more info.
+
+get-content parameters/parameters.prod.json
+
+
 az deployment group create --template-file ./main.bicep  --parameters "parameters/parameters.prod.json" -g "widgets-prod"
