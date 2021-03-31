2e16cbcfd6333cb3ddcb0551d5af4e4fc3d382e2
Buzzology <chrisowens@live.com.au>
2021-03-31 19:29:42 +1000

    Debug

--- Infrastructure/scripts/InvokeDeployment.ps1
+++ Infrastructure/scripts/InvokeDeployment.ps1
@@ -1,10 +1,9 @@
 # This script actually does the deployment. See the readme for more info.
-Write-Host "parameters/parameters.prod.json"
-ls "parameters/*"
+Write-Host "List Files"
 ls *
 
 Write-Host "Getting content:"
-get-content parameters/parameters.prod.json
+get-content "parameters/parameters.prod.json"
 
 
 az deployment group create --template-file ./main.bicep  --parameters "parameters/parameters.prod.json" -g "widgets-prod"
