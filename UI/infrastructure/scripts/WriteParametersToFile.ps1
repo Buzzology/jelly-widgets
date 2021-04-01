d32d7d699ca8d4c7ace9bfa6cbabf05ce247ab27
Buzzology <chrisowens@live.com.au>
2021-04-01 20:49:30 +1000

    d

--- UI/infrastructure/scripts/WriteParametersToFile.ps1
+++ UI/infrastructure/scripts/WriteParametersToFile.ps1
@@ -1,11 +1,11 @@
 # Used to copy environment variables to a file so that they are more accessible to subsequent steps
 $test = "$env:PIPELINE_VARS"
 if($test -eq ""){
-    Write-Host("PIPELINE_VARS not populated")
-    Write-Host("${test}")
+    Write-Host("PIPELINE_VARS not populated")    
     exit 1
 }
 
 Write-Host "Writing vars:"
+Write-Host("${test}")
 
 "$env:PIPELINE_VARS" | Out-File parameters/parameters.prod.json
