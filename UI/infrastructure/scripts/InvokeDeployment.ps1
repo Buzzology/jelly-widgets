a5501a94a2f186887b72135755dd9038821d257d
Buzzology <chrisowens@live.com.au>
2021-04-01 22:03:37 +1000

    d

--- UI/infrastructure/scripts/InvokeDeployment.ps1
+++
@@ -1,2 +0,0 @@
-# This script actually does the deployment. See the readme for more info.
-az deployment group create --template-file ./main.bicep  --parameters "parameters/parameters.prod.json" -g "widgets-prod"
