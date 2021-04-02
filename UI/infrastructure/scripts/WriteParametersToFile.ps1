ef8fd5061a8394343a924d7e76d41ddf867a70fc
Buzzology <chrisowens@live.com.au>
2021-04-02 17:33:27 +1000

    d

--- UI/infrastructure/scripts/WriteParametersToFile.ps1
+++ UI/infrastructure/scripts/WriteParametersToFile.ps1
@@ -1,6 +1,5 @@
 # Used to copy environment variables to a file so that they are more accessible to subsequent steps
-$test = "$env:INPUT_VARS"
-if($test -eq ""){
+if("$env:INPUT_VARS" -eq ""){
     Write-Host("INPUT_VARS not populated")    
     exit 1
 }
